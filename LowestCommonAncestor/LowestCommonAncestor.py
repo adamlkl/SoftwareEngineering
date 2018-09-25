79dada0037b9a8b33444bb926598a42885db53b3
adamlkl <kleong@tcd.ie>
2018-09-25 16:15:24 +0100

    fix minor syntax errors that could create bugs

--- LowestCommonAncestor/LowestCommonAncestor.py
+++ LowestCommonAncestor/LowestCommonAncestor.py
@@ -1,4 +1,3 @@
-#LowestCommonAncestor
 class TreeNode:
     def __init__(self, data):
         self.left = None
@@ -8,25 +7,22 @@ class TreeNode:
 
 class LCA:
 
-    size = 0
-    root = None
-
-    def __init__(self,val):
-        self.root = TreeNode(val)
-        self.size += 1
+    def __init__(self):
+        self.size = 0
+        self.root = None
 
     def isempty(self):
         return self.root is None
 
-    def __sizeof__(self):
+    def __size__(self):
         return self.size
 
-    #TODO
-    def insert(self,data):
+    '#TODO'
+    def insert(self, data):
         if data is None:
             return False
         else:
-            self.__insert(self.root,data)
+            self.__insert(self.root, data)
             return True
 
     def __insert(self, node, data):
@@ -42,26 +38,26 @@ class LCA:
                     node.right = TreeNode(data)
                     self.size += 1
                 else:
-                    self.__insert(node.right,data)
+                    self.__insert(node.right, data)
         else:
-            node.data=data
+            node.data = data
 
+    '# Returns LCA if node a , b are present in the given'
+    '# binary tre otherwise return -1'
+    def findlca(self, a, b):
 
-    # Returns LCA if node a , b are present in the given
-    # binary tre otherwise return -1
-    def findlca(self,a, b):
-
-        if self.isEmpty(): return False
-        # To store paths to a and b from the root
+        if self.isempty():
+            return False
+        '# To store paths to a and b from the root'
         path1 = []
         path2 = []
 
-        # Find paths from root to a and root to b.
-        # If either a or b is not present , return -1
+        '# Find paths from root to a and root to b.'
+        '# If either a or b is not present , return -1'
         if not self.__findpath(self.root, path1, a) or not self.__findpath(self.root, path2, b):
             return -1
 
-            # Compare the paths to get the first different value
+        '# Compare the paths to get the first different value'
         i = 0
         while i < len(path1) and i < len(path2):
             if path1[i] != path2[i]:
@@ -78,16 +74,9 @@ class LCA:
         if node.data == k:
             return True
 
-        if (node.left is None and self.__findpath(node.left, path, k)) or (node.right is None and self.__findpath(node.right, path, k)):
+        if (node.left is None and self.__findpath(node.left, path, k)) or \
+                (node.right is None and self.__findpath(node.right, path, k)):
             return True
 
         path.pop()
         return False
-
-
-
-
-
-
-
-
