1fb2fe1090fba726fe953a3e7f481bed47d74d4a
adamlkl <kleong@tcd.ie>
2018-09-26 13:45:42 +0100

    add extra tests

--- LowestCommonAncestor/test_LCA.py
+++ LowestCommonAncestor/test_LCA.py
@@ -7,9 +7,14 @@ class TestLCA(unittest.TestCase):
         test1 = LCA()
         self.assertEqual(test1.isempty(), True, "Testing if LCA is empty")
 
-    def test_findLCA(self):
+    def test_empty_size(self):
         test2 = LCA()
         self.assertEqual(test2.__size__(), 0, "Testing if size of empty LCA is 0")
+
+    '#TODO testLCA and other functions on empty tree'
+    def test_lca(self):
+        test = LCA()
+        self.assertFalse(test.findlca(4,5))
 
     def testInsert(self):
         test3 = LCA()
@@ -22,6 +27,16 @@ class TestLCA(unittest.TestCase):
         test3.insert(4)
         test3.insert(6)
         self.assertEqual(test3.__size__(), 4, "Testing if size of empty LCA is 1")
+
+    '#TODO test inserting repetitively same data and check size'
+    def testInsert2(self):
+        test = LCA()
+        test.insert(4)
+        test.insert(5)
+        test.insert(6)
+        test.insert(4)
+        test.insert(4)
+        self.assertEqual(test.__size__(), 3, "Testing final size of LCA if it is 3")
 
     def testFindLCA(self):
         test4 = LCA()
@@ -37,6 +52,24 @@ class TestLCA(unittest.TestCase):
         self.assertEqual(test4.findlca(6, 1), 4, "Find LCA if it is 4")
         self.assertEqual(test4.findlca(1, 8), 7, "Find LCA if it is 7")
         self.assertEqual(test4.findlca(10,8), 9, "Find LCA if it is 9")
+
+    '#TODO test LCA on unavailable nodes in the tree'
+    def testFindLCAfail(self):
+        test5 = LCA()
+        test5.insert(7)
+        test5.insert(4)
+        test5.insert(1)
+        test5.insert(5)
+        test5.insert(6)
+        test5.insert(9)
+        test5.insert(10)
+        test5.insert(8)
+        self.assertEqual(test5.findlca(4, 11), -1,"Find unavailable nodes in LCA")
+        self.assertEqual(test5.findlca(13, 11), -1, "Find unavailable nodes in LCA")
+        self.assertEqual(test5.findlca(13, 8), -1, "Find unavailable nodes in LCA")
+
+    '#TODO test num values'
+
 
 if __name__ == '__main__':
     unittest.main()
