877d5314e7d38a7d9aa7e004aaf28455aa01578e
adamlkl <kleong@tcd.ie>
2018-10-10 15:54:28 +0100

    added addvertex, getV,getE, and validate vertex function

--- LowestCommonAncestor/LowestCommonAncestor.py
+++ LowestCommonAncestor/LowestCommonAncestor.py
@@ -8,3 +8,17 @@ class Digraph:
         self.E = int(edges)
         self.digraph = defaultdict(list)
         self.indegree = [0] * self.V
+
+    def add_vertex(self, v, adj):
+        if self.validate_vertex(v) and self.validate_vertex(adj):
+            self.digraph[v].append(adj)
+            self.indegree[adj] += 1
+
+    def get_vertex(self):
+        return self.V
+
+    def get_edge(self):
+        return self.E
+
+    def validate_vertex(self, v):
+        return (type(v) is int) and (v >= 0) and (v < self.V)
